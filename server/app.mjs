88ffcff4f68011636dc9b6db71be13dbf36ff68d
Pudit <mekawinlnwza@gmail.com>
2026-01-16 03:45:10 +0700

    New_01

--- server/app.mjs
+++ server/app.mjs
@@ -2,10 +2,101 @@ import express from "express";
 import connectionPool from "./db.mjs";
 
 const app = express();
-const port = 5000;
+const port = 3123;
+
+
+
+app.use(express.json());
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
+});
+
+app.get("/assignments", async (req, res) => {
+  let assignments;
+  try {
+    const result =  await connectionPool.query("SELECT * FROM assignments");
+    assignments = result.rows;
+  } catch (e) {
+    return res.status(500).json(
+      { "message": "Server could not read assignment because database connection" });
+  }
+  return res.status(200).json({
+  "data": [{ "assignment_data_1": {} }, { "assignment_data_2": {} }, { "assignment_data_3": {} }]
+});
+});
+
+app.get("/assignments/:assignmentId", async (req, res) => {
+    const { assignmentId } = req.params;
+    let assignment;
+      try {
+        const result = await connectionPool.query(
+         "SELECT * FROM assignments WHERE assignment_id = $1",
+          [assignmentId]
+        );
+        assignment = result.rows[0];
+      } catch (e) {
+        return res.status(500).json(
+          { "message": "Server could not read assignment because database connection" });
+      }
+      if (!assignment)
+          return res.status(404).json(
+          { "message": "Server could not find a requested assignment" });
+        return res.status(200).json({
+        "data": { "assignment_data_1": {} } 
+});
+}); 
+
+app.put("/assignments/:assignmentId", async (req, res) => {
+    const { assignmentId } =  req.params;
+    const updatedAssignment = {
+        ...req.body,
+        updated_at: new Date(),
+        published_at: new Date()
+
+    };
+    try {
+      const result = await connectionPool.query(
+        `UPDATE assignments 
+        SET title = $2, content = $3, categorylength = $4, updated_at = $5, published_at = $6, status = $7
+        WHERE assignment_id = $1`,
+        [
+            assignmentId,
+            updatedAssignment.title,
+            updatedAssignment.content,
+            updatedAssignment.categorylength,
+            updatedAssignment.updated_at,
+            updatedAssignment.published_at,
+            updatedAssignment.status
+        ]
+      );
+      if (result.rowCount === 0) {
+          return res.status(404).json(
+          { "message": "Server could not find a requested assignment to update" });
+      }
+  } catch (e) {
+      return res.status(500).json(
+      { "message": "Server could not update assignment because database connection" });
+    }
+    return res.status(200).json({ "message": "Updated assignment successfully" });
+});
+    
+app.delete("/assignments/:assignmentId", async (req, res) => {
+    const { assignmentId } = req.params;
+    try {
+      const result = await connectionPool.query(
+        "DELETE FROM assignments WHERE assignment_id = $1",
+        [assignmentId]
+      );
+      if (result.rowCount === 0) {
+          return res.status(404).json(
+          { "message": "Server could not find a requested assignment to delete" });
+      }
+  } catch (e) {
+      return res.status(500).json(
+      { "message": "Server could not delete assignment because database connection" });
+    }
+    return res.status(200).json({ "message": "Deleted assignment successfully" });
 });
 
 app.post("/assignments", async (req, res) => {
@@ -31,11 +122,12 @@ app.post("/assignments", async (req, res) => {
             newAssignment.status
         ]
       );
-    } catch {
-      return
-      res.status(500).json({ 
-        "message": "Server could not create assignment because database connection" });
+  } catch (e) {
+      return res.status(500)
+      .json({
+      "message": "Server could not create assignment because database connection" });
     }
+  
 
     return res.status(201).json({ "message": "Created assignment successfully" });
 });
--- server/db.mjs
+++ server/db.mjs
@@ -1,9 +1,8 @@
 import * as PG from 'pg';
-
-const { Pool } = PG;
+const { Pool } = PG.default;
 
 const connectionPool = new Pool({
-    connectionString:'postgresql://postgres:pudit2010@localhost:5432/postgres'
+    connectionString:'postgresql://postgres:pudit2010@localhost:5432/postgre_database'
 });
 
 export default connectionPool;
